b5af43d6914e5a553a2cec4eaa0603dab2a1f8ca
HoangNhoo <23120420@student.hcmus.edu.vn>
2025-10-17 19:16:00 +0700

    final

--- user/xargs.c
+++ user/xargs.c
@@ -2,15 +2,67 @@
 #include "user/user.h"
 
 void
-xargs(char* cmd, char** args, int n)
+xargs(char* cmd, char** args)
 {
-  char* argv[22];
+  int curr_argc = 0, new_argc = 0;
+  while(args[curr_argc++])
+    ;
+  curr_argc--;
+  char buf[512];
+  char c;
+  char **new_argv = malloc((curr_argc + 20) * sizeof(char *));
+  for(int i = 0; i < curr_argc; i++)
+    new_argv[i] = args[i];
   int i = 0;
-  while(args[i])
+  new_argc = curr_argc;
+  while(read(0, &c, 1) > 0)
   {
-    argv[i] = args[i];
-    i++;
+    if(c == '\n')
+    {
+      if(i > 0)
+      {
+        buf[i] = '\0';
+        new_argv[new_argc] = malloc(strlen(buf) + 1);
+        strcpy(new_argv[new_argc++], buf);
+        i = 0;
+      }
+      new_argv[new_argc] = 0;
+      int cid = fork();
+      if(cid < 0)
+      {
+        fprintf(2, "xargs: fork failed\n");
+        exit(1);
+      }
+      if(cid == 0)
+      {
+        exec(cmd, new_argv);
+        exit(0);
+      }
+      else
+      {
+        wait(0);
+      }
+      for(int j = curr_argc; j < new_argc; j++)
+        free(new_argv[j]);
+      new_argc = curr_argc;
+    }
+    else {
+      if(c == ' ' || c == '\t')
+      {
+        if(i == 0)
+          continue;
+        buf[i] = '\0';
+        new_argv[new_argc] = malloc(strlen(buf) + 1);
+        strcpy(new_argv[new_argc++], buf);
+        i = 0;
+      }
+      else
+      {
+        buf[i++] = c;
+      }
+    }
   }
+  free(new_argv);
 }
 
 int
@@ -21,24 +73,7 @@ main(int argc, char* argv[])
     fprintf(2, "Usage: xargs command [args...]\n");
     exit(1);
   }
-  //check if command or -n is given
   char* cmd = argv[1];
-  int n = 0;
-  if(strcmp(cmd, "-n") == 0)
-  {
-    if(argc < 4)
-    {
-      fprintf(2, "Usage: xargs -n number command [args...]\n");
-      exit(1);
-    }
-    n = atoi(argv[2]);
-    cmd = argv[3];
-  }
-  if(n < 0 || n + argc - (n > 0 ? 4 : 2) > 20)
-  {
-    fprintf(2, "xargs: invalid number %d (max n : 20)\n", n);
-    exit(1);
-  }
-  xargs(cmd, &argv[1 + (n > 0 ? 3 : 1)], n);
+  xargs(cmd, &argv[1]);
   exit(0);
 }
